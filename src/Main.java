e575c8c3f5cc532e42d4d5d6378cecfc2d1634d2
Salvador <70590860+salvador-palma@users.noreply.github.com>
2023-02-08 20:33:18 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,11 @@ public class Main {
 		// DEFINIR O ESTADO INICIAL AQUI
 		State s = new State(new int[] {1,2,3,4,0,5,6,7,8});	
 		
-		// LargeSearch( Estado Inicial, Index Destino, Numero a meter no Index Destino)
+		//LargeSearch( Estado Inicial, Index Destino, Numero a meter no Index Destino)
 		//Search.LargeSearch(s, 8, 1);
 		
-		Play(s, 8, 1);
+		//Option to play Singleplayer Mode
+		//Play(s, 8, 1);
 		
 	}
 	
